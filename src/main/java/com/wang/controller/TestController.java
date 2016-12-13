fb9354412775497e5dea4f2e4d6e56e4f7da439a
fanggang-wang <fanggang.wang@hpe.com>
2016-12-13 12:39:56 +0800

    index changed

--- src/main/java/com/wang/controller/TestController.java
+++ src/main/java/com/wang/controller/TestController.java
@@ -1,9 +1,13 @@
 package com.wang.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping
@@ -13,6 +17,14 @@ public class TestController {
 	private @ResponseBody String hello(@RequestParam(value = "username", required = false) String username,
 			@RequestParam(value = "password", required = false) String password) {
 		return "Hello " + username + ",Your password is: " + password;
+
+	}
+	
+	@RequestMapping("/index")
+	private ModelAndView index(HttpServletRequest request,HttpServletResponse response) {
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("session", request.getSession().getId());
+		return modelAndView;
 
 	}
 
